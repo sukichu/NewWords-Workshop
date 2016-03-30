3b371adbe5e697ced51b9252bb1282b59d7eb17d
Suki <sukisyx@gmail.com>
2016-03-30 16:22:11 +1100

    Wow, much GIF. Such cool. Wow

--- client/index.js
+++ client/index.js
@@ -7,14 +7,53 @@ myApp.service('HistoryService', function($http){
 		var url = baseUrl + "saveCurrent"
 		return $http.post(url, {"word": newWord})
 	}
+
+	this.getSaved = function(){
+		var url = baseUrl + "getSaved"
+		return $http.get(url)
+	}
 })
 
-myApp.controller('MyController', function($scope, HistoryService){
+myApp.service('GifService', function($http){
+	var baseUrl= "https://api.giphy.com/v1/gifs/"
+	var apiKey = "dc6zaTOxFJmzC"
+
+	this.getGifs = function (query){
+		var url = baseUrl + "search?q=" + query + "&api_key=" + apiKey
+		return $http.get(url)
+	}
+})
+
+myApp.controller('MyController', function($scope, HistoryService,GifService ){
 	$scope.newWord = 'cat'
+	$scope.words = []
+	$scope.gifUrl = ''
 
 	$scope.saveThisWord = function(){
 		HistoryService.saveWord($scope.newWord)
 		.then(saveSuccess, error)
+	}
+
+	$scope.getSavedWords = function(){
+		HistoryService.getSaved()
+		.then(loadSuccess, error)
+	}
+
+	$scope.showGifs = function($event){
+		GifService.getGifs($event.currentTarget.innerHTML)
+		.then(gifSuccess, error)
+	}
+
+	function gifSuccess (json){
+		if(json.data.data[0]){
+			$scope.gifUrl = json.data.data[0].images.fixed_height.url
+		} else{
+			$scope.gifUrl="http://goo.gl/tioFyj"
+		}
+	}
+
+	function loadSuccess (json){
+		$scope.words = json.data
 	}
 
 	function saveSuccess (json){
@@ -25,4 +64,6 @@ myApp.controller('MyController', function($scope, HistoryService){
 		console.log(err)
 	}
 })
+
+
 
--- server/server.js
+++ server/server.js
@@ -8,6 +8,17 @@ var database = new Nedb({filename:'./data/data.db', autoload:true})
 app.use(express.static('../client'))
 app.use(bodyParser.json())
 
+app.get('/getSaved', function(req,res){
+	var query = {}
+
+	var done = function(err, data){
+		console.log('I just read stuff from the database')
+		res.send(data)
+	}
+
+	database.find(query, done)
+})
+
 app.post('/saveCurrent', function(req,res){
 	var data = {word: req.body.word, date: Date.now()}
 
